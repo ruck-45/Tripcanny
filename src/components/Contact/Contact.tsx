f0abd46ccf104e69f75f99229bd496398d829950
Sachin <sachinkaurav65@gmail.com>
2024-01-25 18:28:19 +0530

    change navbar height and font size

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,19 +1,17 @@
-import { Navbar } from '@nextui-org/react'
 import React from 'react'
 import Intro from '../../globalSubComponents/Intro'
-import Footer from '../../globalSubComponents/Footer'
-import Form from '../../globalSubComponents/Form'
-
+import { useDispatch } from "react-redux";
+import { updateTab } from "../../store/curTabSlice";
+import EmailForm from './subElements/EmailForm'
 const title ={des:"Contact Us"}
 
 const Contact = () => {
+  const dispatch = useDispatch();
+  dispatch(updateTab("Contact"));
   return (
     <div>
-        <Navbar/>
         <Intro des={title.des} />
-        <Form/>
-        
-
+        <EmailForm />
     </div>
   )
 }
