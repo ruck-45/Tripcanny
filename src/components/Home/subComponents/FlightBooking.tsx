5529ba3ff109f5661047118e80cef04b9477a2cc
ruck-45 <thoughtsfirstborn01@gmail.com>
2024-02-08 20:02:47 +0530

    added FlightBooking to Home

--- src/components/Home/subComponents/FlightBooking.tsx
+++ src/components/Home/subComponents/FlightBooking.tsx
@@ -1,15 +1,38 @@
 // Dependencies
-import { RadioGroup, Radio, Input, Divider, Select, SelectSection, SelectItem } from "@nextui-org/react";
+import { RadioGroup, Radio, Input, Divider, Select, SelectItem, Button } from "@nextui-org/react";
 import { MdFlightTakeoff, MdFlightLand } from "react-icons/md";
 import { BsFillCalendarDateFill } from "react-icons/bs";
 import { useState } from "react";
+import { IoPeople, IoSend } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { MdEmail } from "react-icons/md";
+import { FaPhone } from "react-icons/fa6";
+
+const flightClass = [
+  {
+    name: "Economy",
+    value: "economy",
+  },
+  {
+    name: "Premium Economy",
+    value: "premium-economy",
+  },
+  {
+    name: "Business Class",
+    value: "business-class",
+  },
+  {
+    name: "First Class",
+    value: "first-class",
+  },
+];
 
 const FlightBooking = () => {
   const [roundTrip, setRoundTrip] = useState(true);
 
   return (
-    <div className="bg-white relative flex justify-center">
-      <div className="w-[80%] p-[3rem] bg-white rounded-xl drop-shadow-xl relative top-[-10rem] flex flex-col gap-[2rem]">
+    <div className="bg-white relative flex justify-center py-[5rem] md:py-0">
+      <div className="w-[80%] p-[3rem] bg-white rounded-xl drop-shadow-xl relative md:top-[-7rem] flex flex-col gap-[2rem]">
         <RadioGroup
           orientation="horizontal"
           color="danger"
@@ -28,6 +51,13 @@ const FlightBooking = () => {
             <Input type="text" label="From" startContent={<MdFlightTakeoff />} />
             <Input type="text" label="to" startContent={<MdFlightLand />} />
             <Input type="date" label="date" startContent={<BsFillCalendarDateFill />} />
+            <Select label="Class" defaultSelectedKeys={["economy"]} className="max-w-xs">
+              {flightClass.map((data) => (
+                <SelectItem value={data.value} key={data.value}>
+                  {data.name}
+                </SelectItem>
+              ))}
+            </Select>
           </div>
           <Divider className="mt-[1rem]" />
           {roundTrip ? (
@@ -37,10 +67,33 @@ const FlightBooking = () => {
                 <Input type="text" label="From" startContent={<MdFlightTakeoff />} />
                 <Input type="text" label="to" startContent={<MdFlightLand />} />
                 <Input type="date" label="date" startContent={<BsFillCalendarDateFill />} />
+                <Select label="Class" defaultSelectedKeys={["economy"]} className="max-w-xs">
+                  {flightClass.map((data) => (
+                    <SelectItem value={data.value} key={data.value}>
+                      {data.name}
+                    </SelectItem>
+                  ))}
+                </Select>
               </div>
               <Divider className="mt-[1rem]" />
             </>
           ) : null}
+          <p className="text-default-500">Personal Information</p>
+          <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-[2rem]">
+            <Input type="number" label="Passengers" defaultValue="1" startContent={<IoPeople />} />
+            <Input type="text" label="Name" startContent={<CgProfile />} />
+            <Input type="email" label="Email" startContent={<MdEmail />} />
+            <Input type="phone" label="Phone" startContent={<FaPhone />} />
+          </div>
+          <Button
+            color="warning"
+            variant="shadow"
+            type="submit"
+            className="text-white self-center mt-[2rem]"
+            endContent={<IoSend className="mt-[0.2rem]" />}
+          >
+            Submit
+          </Button>
         </form>
       </div>
     </div>
