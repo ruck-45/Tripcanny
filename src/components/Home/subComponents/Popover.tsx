b5410633f2fe5906a90d3c0a5b563d2657a7722d
ruck-45 <thoughtsfirstborn01@gmail.com>
2024-02-20 13:37:50 +0530

    popover resize

--- src/components/Home/subComponents/Popover.tsx
+++ src/components/Home/subComponents/Popover.tsx
@@ -3,8 +3,6 @@ import { Modal, ModalContent, ModalHeader, ModalBody, Button, useDisclosure, Ima
 import { useRef, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { FaPhone } from "react-icons/fa6";
-import { BiSolidPhoneCall } from "react-icons/bi";
-import { MdAddIcCall } from "react-icons/md";
 
 // Local Files
 import "./Popover.css";
@@ -44,8 +42,8 @@ const Popover = () => {
               <p>Contact Us</p>
             </ModalHeader>
             <ModalBody className="flex flex-col justify-center items-center  py-[1rem]">
-              <Image width={100} src={logo} alt="logo" radius="none" />
-              <h1 className="text-[1.5rem] leading-[2.2rem] text-center font-bold">24/7 Reservation & Support</h1>
+              <Image width={50} src={logo} alt="logo" radius="none" />
+              <h1 className="text-[1.3rem] leading-[2.2rem] text-center font-bold">24/7 Reservation & Support</h1>
               <div className="grid grid-cols-2 gap-[0.5rem]">
                 {popbtns.map((data, index) => (
                   <Button
@@ -59,23 +57,15 @@ const Popover = () => {
               </div>
 
               <div
-                className="w-[10rem] h-[10rem] rounded-full bg-no-repeat bg-center bg-cover border-black border-[0.2rem]"
+                className="w-[7rem] h-[7rem] rounded-full bg-no-repeat bg-center bg-cover border-black border-[0.2rem]"
                 style={{
                   backgroundImage:
                     "url(https://images.pexels.com/photos/7709235/pexels-photo-7709235.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
                 }}
               ></div>
-              <h1 className="text-[1.2rem] leading-[2.2rem] text-center font-semibold">
+              <h1 className="text-[1.1rem] leading-[2rem] text-center font-semibold">
                 Call & Get Unpublished Flight Deals
               </h1>
-
-              <Button
-                className="text-[1rem] font-bold bg-black border text-white self-center px-[1.5rem] py-[1rem]"
-                radius="full"
-                startContent={<FaPhone />}
-              >
-                +1 (888) 891-7176
-              </Button>
 
               <h1 className="text-[1.5rem] leading-[2.2rem] text-center font-bold">24/7 Helpline Available</h1>
               <h1 className="text-[1.1rem] text-center leading-[1.2rem]">
